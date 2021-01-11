cdb92d9511fada22a310bfd1c54f309f1d06a737
Braindead <albertopoljak@gmail.com>
2021-01-11 19:35:21 +0100

    Restructured message_boxes.py

--- mc_fart_mic/message_boxes.py
+++ mc_fart_mic/message_boxes.py
@@ -3,25 +3,31 @@ import traceback
 from PyQt5.QtWidgets import QMessageBox
 
 
-def _message_box_constructor(message_text: str, *, title: str, icon: QMessageBox.Icon = None) -> QMessageBox:
+def message_box_constructor(
+        message_text: str,
+        *,
+        title: str, informative_text: str = None,
+        detailed_text: str = None, icon: QMessageBox.Icon = QMessageBox.Information
+) -> QMessageBox:
     message_box = QMessageBox()
     message_box.setWindowTitle(title)
     message_box.setText(message_text)
-    if icon is not None:
-        message_box.setIcon(icon)
+    message_box.setInformativeText(informative_text)
+    message_box.setDetailedText(detailed_text)
+    message_box.setIcon(icon)
     return message_box
 
 
 def show_simple_info_message(message_text: str):
-    _message_box_constructor(message_text, title="Info").exec_()
+    message_box_constructor(message_text, title="Info").exec_()
 
 
 def show_simple_success_message(message_text: str):
-    _message_box_constructor(message_text, title="Success", icon=QMessageBox.Information).exec_()
+    message_box_constructor(message_text, title="Success").exec_()
 
 
 def show_simple_warning_message(message_text: str):
-    _message_box_constructor(message_text, title="Warning", icon=QMessageBox.Warning).exec_()
+    message_box_constructor(message_text, title="Warning", icon=QMessageBox.Warning).exec_()
 
 
 def show_simple_traceback_message(message_text: str, *, custom_traceback_msg: str = None):
@@ -29,12 +35,14 @@ def show_simple_traceback_message(message_text: str, *, custom_traceback_msg: st
     Shows information on the last traceback.
     If custom_traceback_msg is passed then shows that instead of last traceback.
     """
-    error_msg = _message_box_constructor(message_text, title="Error", icon=QMessageBox.Warning)
-    error_msg.setDetailedText(traceback.format_exc(1) if custom_traceback_msg is None else custom_traceback_msg)
+    traceback_msg = traceback.format_exc(1) if custom_traceback_msg is None else custom_traceback_msg
+    error_msg = message_box_constructor(
+        message_text, title="Error", icon=QMessageBox.Warning, detailed_text=traceback_msg
+    )
     error_msg.exec_()
 
 
 def show_simple_confirmation_message(message_text: str = "Are you sure?") -> bool:
-    confirmation_msg = _message_box_constructor(message_text, title="Confirmation", icon=QMessageBox.Question)
+    confirmation_msg = message_box_constructor(message_text, title="Confirmation", icon=QMessageBox.Question)
     confirmation_msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
     return confirmation_msg.exec_() == QMessageBox.Ok
